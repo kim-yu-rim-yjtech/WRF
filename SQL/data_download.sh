2f37f1b73a1c5fdbe62e927b9d33fc426b4a8d86
kim-yu-rim-yjtech <yrkim@youngjintech.com>
2024-11-11 17:18:19 +0900

    data_download.sh 파일 수정

--- SQL/data_download.sh
+++ SQL/data_download.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+exec 2>error_log.txt  # 모든 오류 출력을 error_log.txt에 저장
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y gcc gfortran g++ libtool automake autoconf make m4 grads default-jre csh
@@ -11,8 +12,8 @@ export $(grep -v '^#' .env | xargs)
 echo "Current Environment: $ENV"
 echo "HOME directory: $HOME"
 
-mkdir $HOME/CALLPUFF/WRF
-cd ../CALPUFF/WRF
+# mkdir $HOME/WRF
+cd $HOME/WRF
 mkdir Downloads
 mkdir Library
 
@@ -26,7 +27,7 @@ wget -c http://www.mpich.org/static/downloads/3.3.1/mpich-3.3.1.tar.gz
 wget -c https://download.sourceforge.net/libpng/libpng-1.6.37.tar.gz
 wget -c https://www.ece.uvic.ca/~frodo/jasper/software/jasper-1.900.1.zip
 
-export DIR=$HOME/CALPUFF/WRF/Library
+export DIR=$HOME/WRF/Library
 export CC=gcc
 export CXX=g++
 export FC=gfortran
@@ -40,7 +41,7 @@ make
 make install
 
 ## HDF5 설치
-cd ../
+cd $HOME/WRF/Downloads
 tar -xvzf hdf5-1.10.5.tar.gz
 cd hdf5-1.10.5/
 ./configure --prefix=$DIR --with-zlib=$DIR --enable-hl --enable-fortran
@@ -51,7 +52,7 @@ export HDF5=$DIR
 export LD_LIBRARY_PATH=$DIR/lib:$LD_LIBRARY_PATH
 
 ## netCDF - c 설치
-cd ../
+cd $HOME/WRF/Downloads
 tar xvzf netcdf-c-4.9.0.tar.gz
 cd netcdf-c-4.9.0
 export CPPFLAGS=-I$DIR/include 
@@ -64,7 +65,7 @@ export PATH=$DIR/bin:$PATH
 export NETCDF=$DIR
 
 ## netCDF - fortran 설치
-cd ../
+cd $HOME/WRF/Downloads
 tar -xvzf netcdf-fortran-4.6.0.tar.gz
 cd netcdf-fortran-4.6.0/
 export LD_LIBRARY_PATH=$DIR/lib:$LD_LIBRARY_PATH
@@ -76,7 +77,7 @@ make check
 make install
 
 ## MPICH 설치
-cd ../
+cd $HOME/WRF/Downloads
 tar -xvzf mpich-3.3.1.tar.gz
 cd mpich-3.3.1/
 ./configure --prefix=$DIR
@@ -84,7 +85,7 @@ make
 make install
 
 ## libpng 설치
-cd ../ 
+cd $HOME/WRF/Downloads
 export LDFLAGS=-L$DIR/lib
 export CPPFLAGS=-I$DIR/include
 tar -xvzf libpng-1.6.37.tar.gz
@@ -94,7 +95,7 @@ make
 make install
 
 ## jasper 설치
-cd ../
+cd $HOME/WRF/Downloads
 sudo apt install unzip
 sudo apt update
 unzip jasper-1.900.1.zip
@@ -108,10 +109,10 @@ export JASPERLIB=$DIR/lib
 export JASPERINC=$DIR/include
 
 # WRF 모델 설치
-cd ../../
+cd $HOME/WRF/
 wget -c https://github.com/wrf-model/WRF/archive/v4.1.2.tar.gz
 tar -xvzf v4.1.2.tar.gz -C $HOME/WRF
-cd ../WRF-4.1.2/
+cd $HOME/WRF/WRF-4.1.2/
 ./clean
 
 expect << EOF
@@ -124,10 +125,10 @@ EOF
 export WRF_DIR=$HOME/WRF/WRF-4.1.2
 
 # WPS 모델 설치
-cd ../
+cd $HOME/WRF/
 wget -c https://github.com/wrf-model/WPS/archive/v4.1.tar.gz
 tar -xvzf v4.1.tar.gz -C $HOME/WRF
-cd ../WPS-4.1/
+cd $HOME/WRF/WPS-4.1/
 
 expect << EOF
 spawn ./configure
@@ -137,11 +138,15 @@ EOF
 ./compile
 
 # geog 데이터 다운로드
-cd ../
+cd $HOME/WRF/
 mkdir WPS_GEOG
 pip show requests &> /dev/null || pip install requests
 pip show beautifulsoup4 &> /dev/null || pip install beautifulsoup4
 pip show nest_asyncio &> /dev/null || pip install nest_asyncio
 pip show asyncio &> /dev/null || pip install asyncio
 pip show flask &? /dev/null || pip install flask
-python3 download_geog_data.py
+python3 $HOME/WRF/download_geog_data.py
+mv $HOME/WRF/static/geog_high_res_mandatory.tar.gz $HOME/WRF/WPS_GEOG
+
+
+# grib2 데이터 다운로드
